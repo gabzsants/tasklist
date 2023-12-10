57ba53e9eac804cb2dc337bf697887e50f23f519
Gabriela S. Duraes <138320933+gabzsants@users.noreply.github.com>
2023-12-10 03:14:49 -0300

    Atualizar o script.js
    versão atualizada com botões editar e deletar funcionando

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var colors = ['#0000FF', '#FF1493', '#DC143C', '#FF8C00', '#7B68EE', '#32CD32', '#A020F0', '#FFD700'];
 
-document.getElementById('Formulario').addEventListener('submit', function(event) {
+  document.getElementById('Formulario').addEventListener('submit', function (event) {
     event.preventDefault();
 
     var tituloTarefa = document.getElementById('tituloTarefa').value;
@@ -9,16 +9,36 @@ document.getElementById('Formulario').addEventListener('submit', function(event)
     var sticky = document.createElement('div');
     sticky.className = 'sticky';
     sticky.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-    sticky.innerHTML = '<h4>' + tituloTarefa + '</h4><p>' + descricaoTarefa + '</p><button class="botao-concluir" onclick="completeTask(this)">Marcar como Concluída</button>';
+    sticky.innerHTML = '<h4>' + tituloTarefa + '</h4><p>' + descricaoTarefa + '</p>' +
+      '<button class="botao-editar" onclick="editTask(this)">Editar</button>' +
+      '<button class="botao-remover" onclick="removeTask(this)">Remover</button>l' + 
+      '<button class="botao-concluir" onclick="completeTask(this)">Marcar como Concluída</button>';
 
     document.getElementById('tarefasAndamento').appendChild(sticky);
-});
 
-function completeTask(button) {
+    document.getElementById('tituloTarefa').value = '';
+    document.getElementById('descricaoTarefa').value = '';
+  });
+
+  function completeTask(button) {
     var sticky = button.parentNode;
     document.getElementById('tarefasCompletas').appendChild(sticky);
     button.remove();
-}
+  }
 
+  function removeTask(button) {
+    var sticky = button.parentNode;
+    sticky.remove();
+  }
 
+  function editTask(button) {
+    var sticky = button.parentNode;
 
+    var titulo = sticky.querySelector('h4').innerText;
+    var descricao = sticky.querySelector('p').innerText;
+
+    document.getElementById('tituloTarefa').value = titulo;
+    document.getElementById('descricaoTarefa').value = descricao;
+
+    sticky.remove();
+  }
